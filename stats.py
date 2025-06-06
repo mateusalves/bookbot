5af0e396d4dbc8b54a8d0bb00537b6c700cdd87e
Mateus Alves <mateus.alves.unb@gmail.com>
2025-06-06 10:01:22 -0300

    Move print characters to stats

--- stats.py
+++ stats.py
@@ -15,6 +15,12 @@ def print_num_words(file_contents):
     print(f"Found {num_words} total words")
 
 
+def print_characters(sorted_dict):
+    for k, v in sorted_dict.items():
+        if k.isalpha():
+            print(f"{k}: {v}")
+
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
